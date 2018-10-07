b7afbb7e7d9465c36c9c07ee7fad39f54d765fef
Graf2242 <graf2242@gmail.com>
2018-10-07 16:07:32 +0300

    Add calculation md5

--- homework.py
+++ homework.py
@@ -1,4 +1,8 @@
+import hashlib
+
 my_name = "d.galamaga"
+m = hashlib.md5()
+m.update(my_name.encode())
 
 if __name__ == "__main__":
-    print(f"Task completed by {my_name}!")
+    print(f"Task completed by {my_name}! md5 is {m.hexdigest()}")
